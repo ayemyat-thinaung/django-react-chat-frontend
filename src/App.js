8c7a715996fd6a3bd18c88839de0d0a90954f0c2
Aye Myat Thin Aung <ayemyatthinaung@achromex.com>
2020-01-02 13:37:27 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { BrowserRouter as Router, Switch } from "react-router-dom";
+// import Chat from "./Chat";
+// import Login from "./Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import UnauthedRoute from "./UnauthedRoute";
+// import AuthedRoute from "./AuthedRoute";
+
+// const App = () => (
+//   <Router>
+//     <Switch>
+//       <UnauthedRoute path="/auth/login" component={Login} />
+//       <AuthedRoute path="/" component={Chat} />
+//     </Switch>
+//   </Router>
+// );
+
+// export default App;
+import React from "react";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  Thread,
+  Window
+} from "stream-chat-react";
+import { MessageList, MessageInput } from "stream-chat-react";
+import { StreamChat } from "stream-chat";
+
+import "stream-chat-react/dist/css/index.css";
+
+const chatClient = new StreamChat("qk4nn7rpcn75"); // Demo Stream Key
+const userToken =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY29vbC1za3ktOSJ9.mhikC6HPqPKoCP4aHHfuH9dFgPQ2Fth5QoRAfolJjC4"; // Demo Stream Token
+
+chatClient.setUser(
+  {
+    id: "cool-sky-9",
+    name: "Cool sky",
+    image: "https://getstream.io/random_svg/?id=cool-sky-9&name=Cool+sky"
+  },
+  userToken
+);
+
+const channel = chatClient.channel("messaging", "godevs", {
+  // image and name are required, however, you can add custom fields
+  image:
+    "https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png",
+  name: "Talk about Go"
+});
+
+const App = () => (
+  <div style={{ width: 100, height: 100 }}>
+    <Chat client={chatClient} theme={"messaging light"}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  </div>
+);
 
 export default App;
